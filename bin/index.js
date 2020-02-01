910b25fb2d606ce55cc06270760211a66821c6a0
Oscar Childs <oscar.childs@bionical.com>
2020-02-01 18:36:22 +0000

    reformat table output

--- bin/index.js
+++ bin/index.js
@@ -19,7 +19,9 @@ var getMenu = function () {
         } else {
           console.log('Get successful!\n')
 
-          console.table(data.menu)
+          var menuTable = data.menu.reduce((acc, {day, ...x}) => { acc[day] = x; return acc}, {})
+
+          console.table(menuTable)
         }
     })
     .auth(
@@ -29,7 +31,7 @@ var getMenu = function () {
 }
 
 if (process.argv.length < 3) {
-  console.log('please provide a password using \"dock-menu\" <password>')
+  console.log('please provide a password using \"dock-menu <password>\"')
 } else {
   getMenu()
 }
